d7305904fc234c5597e18c4c4760bc5832791940
Igor Forner <igorfornerrodrigues2@gmail.com>
2025-06-13 19:12:16 -0300

    add body

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
+import { Body } from "./components/body/body";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Body],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
