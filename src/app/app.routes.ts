5dc65d00efe4d188373a2ad38ecd341acb2a7a8e
Igor Forner <igorfornerrodrigues2@gmail.com>
2025-07-09 13:50:09 -0300

    Add About/Footer and fixes body

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Body } from './components/body/body';
+import { About } from './components/about/about';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "home",component: Body},
+  {path: "about", component: About}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
-import { Body } from "./components/body/body";
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Body],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
